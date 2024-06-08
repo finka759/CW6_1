6643908f3e1dd7ab2ac2a0a9b85321c98c116c38
finka759 <finka759@gmail.com>
2024-06-08 22:35:01 +0300

    работает

--- mailing/urls.py
+++ mailing/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from mailing.apps import MailingConfig
 from mailing.views import MailingParametersCreateView, MailingParametersListView, MailingParametersDetailView, \
-    MailingParametersUpdateView
+    MailingParametersUpdateView, MailingParametersDeleteView, MessageListView, MessageCreateView, MessageUpdateView
 
 app_name = MailingConfig.name
 
@@ -9,7 +9,10 @@ urlpatterns = [
     path('mailingparameters_list/', MailingParametersListView.as_view(), name='mp_list'),
     path('', MailingParametersListView.as_view(), name='mp_list'),
     path('create/', MailingParametersCreateView.as_view(), name='create'),
-    path('mp_detail/<int:pk>/',  MailingParametersDetailView.as_view(), name='mp_detail'),
+    path('mp_detail/<int:pk>/', MailingParametersDetailView.as_view(), name='mp_detail'),
     path('edit/<int:pk>/', MailingParametersUpdateView.as_view(), name='edit'),
-    # path('create/', Client___View.as_view(), name='delete'),
+    path('delete/<int:pk>/', MailingParametersDeleteView.as_view(), name='delete'),
+    path('message_list/', MessageListView.as_view(), name='message_list'),
+    path('message_create/', MessageCreateView.as_view(), name='message_create'),
+    path('message_edit/<int:pk>/', MessageUpdateView.as_view(), name='message_edit'),
 ]
